972430b69d2262c265521ca1384c2e4149b86b03
ashishCloudesign <ashish.saini@cloudesign.com>
2024-04-18 20:48:17 +0530

    continue without login added

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useAuth0 } from 'react-native-auth0';
 import { globalstyle } from '../assets/styles/gloabalstyles';
 import { debouncingFunc } from './debouncingFunc';
@@ -8,9 +8,12 @@ function Login({ navigation }) {
     const { authorize, user } = useAuth0();
 
     console.log({ user })
+    const navigateToHome = () => {
+        navigation.navigate('Home')
+    }
     useEffect(() => {
         if (user) {
-            navigation.navigate('Home')
+            navigateToHome()
         }
     }, [user])
 
@@ -24,21 +27,27 @@ function Login({ navigation }) {
 
     return (
         <View style={{ flex: 1 }}>
-            <View style={{ position: 'absolute', bottom: 0, justifyContent: 'flex-end', width: '100%' }}>
-                <TouchableOpacity onPress={() => debouncingFunc(onPress, 1000)}
-                    style={{ ...style.buttonContainer }}
-                >
-                    <Text style={{
-                        fontFamily: globalstyle.font.fontFamily,
-                        textAlign: 'center',
-                        fontSize: 17,
-                        marginTop: 5,
-                        color: 'white',
-                        ...style.buttonText
+            {/* <Image style={style.img1} source={require('../assets/Images/logo.png')} /> */}
+            <ImageBackground source={require('../assets/Images/splash.png')} resizeMode="cover" style={style.img1}>
+                <Text
+                    onPress={() =>
+                        navigation.navigate('Home')}
+                    style={{
+                        ...style.WithoutLogin
                     }}>
-                        Sign In</Text>
-                </TouchableOpacity>
-            </View>
+                    Continue Without Login</Text>
+                <View style={{ position: 'absolute', bottom: 0, justifyContent: 'flex-end', width: '100%' }}>
+                    <TouchableOpacity onPress={() => debouncingFunc(onPress, 1000)}
+                        style={{ ...style.buttonContainer }}
+                    >
+                        <Text style={{
+
+                            ...style.buttonText
+                        }}>
+                            Sign In</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
         </View>
     )
 }
@@ -47,15 +56,35 @@ export default Login
 
 const style = StyleSheet.create({
     buttonContainer: {
-        backgroundColor: 'green',
+        backgroundColor: 'white',
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
     },
     buttonText: {
-        color: '#fff',
+        color: 'black',
         fontSize: 16,
         fontWeight: 'bold',
-    },
+        fontFamily: globalstyle.font.fontFamily,
+        textAlign: 'center',
+        fontSize: 17,
+        marginTop: 5,
+    }, WithoutLogin: {
+        fontSize: 16,
+        // fontWeight: 'bold',
+        // fontFamily: globalstyle.font.fontFamily,
+        textAlign: 'center',
+        color: 'white',
+        fontStyle: 'italic',
+        // marginTop: 150,
+        fontWeight: 200,
+        position: 'absolute',
+        bottom: "8%",
+        left: "30%"
+
+    }, img1: {
+        flex: 1,
+        justifyContent: 'center',
+    }
 })
 
